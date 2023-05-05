d88d98cb6eefb44ebe4d4e27354192b56441dd8f
PatoTacchella <patricia.tacchella@gmail.com>
2023-05-05 16:19:06 -0700

    up date

--- Soldado/main.cpp
+++ Soldado/main.cpp
@@ -1,68 +1,105 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 using namespace std;
+
+
+
+class Armas
+{
+protected:
+	string name = NULL;
+	string disparo = NULL;
+
+public:
+	Armas() { };
+	~Armas() { };
+	void setArma() {};
+	virtual void disparar();
+	string getArma() { return name; };
+};
+
+class Revolver : public Armas
+{
+	Revolver() : Armas() {
+		name = "Revolver";
+		disparo = "Pum";
+	};
+	~Revolver() { };
+	void disparar() { cout << disparo << "\n"; };
+};
+
+class Rifle : public Armas
+{
+	Rifle() : Armas () {
+		name = "Rifle";
+		disparo = "Pum pum pum..";
+	};
+	~Rifle() { };
+	void disparar() {
+		cout << disparo << "\n";
+	};
+};
+
+class Escopeta : public Armas
+{
+	Escopeta() : Armas() {
+		name = "Escopeta";
+		disparo = "Paaa...";
+	};
+	~Escopeta() { };
+	void disparar() {
+		cout << disparo << "\n";
+	};
+};
 
 class Soldado
 {
 	Armas* _arma;
 
 public:
-	Soldado() { _arma = NULL; }; 
-	//~Soldado() { cout << "borrado"; delete Soldado; };
+	Soldado() { cout << "creando"; };
+	~Soldado() { cout << "borrado"; };
 
-	void elegirArma(int n);
+	void elegirArma();
 	bool tieneArma();
 	void dejarArma();
 	void armaEnUso();
 	void gatillar();
 };
 
-class Armas
+void Soldado::elegirArma()
 {
-public:
-	string arma;
-	
-	Revolver* _revolver;
-	Rifle* _rifle;
-	Escopeta* _escopeta;
-
-	//Armas() { arma = NULL; };
-	//~Armas() { cout << "borrar arma"; delete Armas; };
-
-	void disparar() { /*cada arma dispara distinto*/ };
-};
-
-class Revolver : public Armas
-{
-	
-};
-
-class Rifle : public Armas
-{
-
-};
-
-class Escopeta : public Armas
-{
-
-};
-
-
-void Soldado::elegirArma(int n)
-{
+	int n;
 	cout << "Por favor, escoja el arma que desea utilizar: \n"
 		<< "\n"
 		<< "1. Revolver\n"
 		<< "2. Rifle\n"
 		<< "3. Escopeta\n"
 		<< "0. Volver atras ";
-	
+	cin >> n;
+
 	switch (n)
 	{
-	case 1: { _arma = _arma->_revolver; } break;
-	case 2: { _arma = _arma->_rifle; } break;
-	case 3: { _arma = _arma->_escopeta; } break;
-	case 0: {  /* salir */ } break;
+	case 1: { 
+		Revolver* revolver = new Revolver();
+		_arma = revolver;
+		cout << "Usted eligio: " << revolver->getArma();
+		//SALIR  n = 0;
+	} break;
+	case 2: { 
+		Rifle* rifle;
+		_arma = rifle;
+		cout << "Usted eligio: " << rifle->getArma();
+		//salir
+	} break;
+	case 3: { 
+		Escopeta* escopeta;
+		_arma = escopeta;
+		cout << "Usted eligio: " << escopeta->getArma();
+		//salir
+	} break;
+	case 0: {  /* SALIR */ } break;
 	}
 
 }
@@ -103,6 +140,10 @@ void Soldado::armaEnUso()
 
 
 
+void Armas::disparar()
+{
+	cout << disparo << "\n";
+}
 
 
 
@@ -110,108 +151,35 @@ void Soldado::armaEnUso()
 
 int main()
 {
+
 	Soldado* sold = new Soldado();
-
-
-	string entradaPrin;
-	
-	//while (entradaPrin != "X")
-	//{
-	//	
-
-	//	cin >> int entradaPrin;
-
-	//	switch (entradaPrin)
-	//	{
-	//	default:
-	//	{
-	//		cout << " -== Bienvenido al campo de entrenamiento, Soldado ==- \n\n";
-
-	//		cout << "Que desea hacer? \n"
-	//			<< "(presione el numero correspondiente a la opcion del menu)\n"
-	//			<< "\n"
-	//			<< "1. Recoger arma\n"
-	//			<< "2. Dejar arma\n"
-	//			<< "3. Disparar\n"
-	//			<< "4. Ver arma en uso\n"
-	//			<< "0. Salir";
-	//	}
-	//		break;
-	//	case 1: 
-	//	{
-	//		if (sold->tieneArma())
-	//		{
-	//			int eleccion;
-
-	//			cout << "Por favor, escoja el arma que desea utilizar: \n"
-	//				<< "\n"
-	//				<< "1. " << arma->Escopeta << "\n"
-	//				<< "2. " << arma->Revolver<< "\n"
-	//				<< "3. " << arma->Rifle << "\n"
-	//				<< "0. Volver atras ";
-	//			
-	//			//arma->elegirArma(cin >> eleccion);
-	//			
-	//			if (eleccion == 0)
-	//			{
-	//				entradaPrin = default;
-	//			}
-
-	//		}
-	//		else 
-	//		{
-	//			cout << "Usted ya posee un arma.. \n"
-	//				<< "\n"
-	//				<< "Presione X para volver para atras";
-	//			//implementar comandos
-	//			
-	//		}
-	//	}
-	//		break;
-
-	//	case 2:
-	//	{
-	//		cout << "Usted posee: " << arma << "\n"
-	//			<< "\n"
-	//			<< "Precione ENTER, para dejar su arma y volver al menu anterior";
-	//		
-	//		// arma = NULL 
-	//		//volver para atras
-
-	//	}
-	//		break;
-
-	//	case 3: 
-	//	{
-	//		if (sold->tieneArma())
-	//		{
-	//			cout << " Usted a disparado.. ";
-	//		}
-	//		else
-	//		{
-	//			cout << "usted no tiene arma, debe elegir una antes de disparar \n"
-	//				<< "presione Y para ir al menu de armas.. ";
-	//			// implementar if Y >> a armas 
-	//			// si no x o enter e ir a menu general
-	//		}
-	//	}
-	//		break;
-
-	//	case 4:
-	//	{
-	//		if (sold->tieneArma())
-	//		{
-	//			cout << " Usted esta equipando: " << arma;
-	//		}
-	//		else
-	//		{
-	//			cout << " Usted no esta quipando arma";
-	//		}
-	//		// como salgo?
-	//	}
-	//	break;
-	//	}
-	//}
 	
 
+	int opcion;
+	
+	
+	
+	switch (opcion)
+	{
+	default:
+	{
+		cout << " -== Bienvenido al campo de entrenamiento, Soldado ==- \n\n";
+
+		cout << "Que desea hacer? \n"
+			<< "(presione el numero correspondiente a la opcion del menu)\n"
+			<< "\n"
+			<< "1. Recoger arma\n"
+			<< "2. Dejar arma\n"
+			<< "3. Disparar\n"
+			<< "4. Ver arma en uso\n"
+			<< "0. Salir";
+	}
+	break;
+
+	case 1:
+	{		
+		;
+	}
+	break;
+	}
 }
